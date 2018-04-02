13fa0daff6958d3c242918b00c80ec1f63b481a5
Sebastian Schmidt <publicarray@users.noreply.github.com>
2018-04-02 17:38:45 +1000

    fix formatting, and delete extra image

--- spk/nmap/src/service-setup.sh
+++ spk/nmap/src/service-setup.sh
@@ -1,10 +1,10 @@
-service_postinst () {
-    # Put nmap in the PATH
+service_postinst ()
+{
     mkdir -p /usr/local/bin
     ln -s /var/packages/"${SYNOPKG_PKGNAME}"/target/bin/nmap /usr/local/bin/nmap
 }
 
-service_postuninst () {
-    # Remove link
+service_postuninst ()
+{
     rm -f /usr/local/bin/nmap
 }

cf3cc55f49289d1be28b99f6b0cbc9a345627cc3
Sebastian Schmidt <publicarray@users.noreply.github.com>
2018-04-02 18:42:29 +1000

    code review

--- spk/dnscrypt-proxy/src/dsm-control.sh
+++ spk/dnscrypt-proxy/src/dsm-control.sh
@@ -10,20 +10,20 @@ PID_FILE=/usr/local/run/dnscrypt-proxy.pid
 
 start ()
 {
-    # su ${USER} -s /bin/sh -c "PATH=${PATH} ${DNSCRYPT_PROXY} -f ${CFG_FILE}"
+    # su ${USER} -s /bin/sh -c "PATH=${PATH} ${DNSCRYPT_PROXY} --config ${CFG_FILE} & echo $! > ${PID_FILE}"
     ${DNSCRYPT_PROXY} --config ${CFG_FILE} & echo $! > ${PID_FILE}
 }
 
 stop ()
 {
-    kill `cat ${PID_FILE}`
-    wait_for_status 1 20 || kill -9 `cat ${PID_FILE}`
+    kill "$(cat ${PID_FILE})"
+    wait_for_status 1 20 || kill -9 "$(cat ${PID_FILE})"
     rm -f ${PID_FILE}
 }
 
 status ()
 {
-    if [ -f ${PID_FILE} ] && kill -0 `cat ${PID_FILE}` > /dev/null 2>&1; then
+    if [ -f ${PID_FILE} ] && kill -0 "$(cat ${PID_FILE})" > /dev/null 2>&1; then
         return
     fi
     rm -f ${PID_FILE}
@@ -33,9 +33,9 @@ status ()
 wait_for_status ()
 {
     counter=$2
-    while [ ${counter} -gt 0 ]; do
+    while [ "${counter}" -gt 0 ]; do
         daemon_status
-        [ $? -eq $1 ] && return
+        [ $? -eq "$1" ] && return
         let counter=counter-1
         sleep 1
     done

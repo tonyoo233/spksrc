4500803be9d0f67295c8e931148a0df38d6f4ef4
Sebastian Schmidt <publicarray@users.noreply.github.com>
2018-04-13 10:11:34 +1000

    Cleanup :boom:

--- spk/dnscrypt-proxy/src/ui/cgi.go
+++ spk/dnscrypt-proxy/src/ui/cgi.go
@@ -5,17 +5,10 @@ import (
     "os"
     "os/exec"
     "errors"
-    // "net/http/cgi"
     "html/template"
-    // "net/http"
     "regexp"
     "github.com/BurntSushi/toml"
 )
-
-type PageData struct {
-    Title string
-    ListenAddresses []string
-}
 
 // ref: https://github.com/jedisct1/dnscrypt-proxy/blob/master/dnscrypt-proxy/config.go
 type Config struct {
@@ -25,48 +18,24 @@ type Config struct {
     SourceIPv6 bool `toml:"ipv6_servers"`
 }
 
-var dev = false
-
-// func cgihandler(w http.ResponseWriter, r *http.Request) {
-
-//     header := w.Header()
-//     // header.Set("Content-Type", "text/plain; charset=utf-8") // when something goes wrong
-//     header.Set("Content-Type", "text/html; charset=utf-8")
-
-//     var conf Config
-//     // if _, err := toml.DecodeFile("example-dnscrypt-proxy.toml", &conf); err != nil { // for development
-//     if _, err := toml.DecodeFile("/var/packages/dnscrypt-proxy/target/var/dnscrypt-proxy.toml", &conf); err != nil {
-//         http.Error(w, err.Error(), http.StatusInternalServerError)
-//         return
-//     }
-
-//     tmpl, err := template.ParseFiles("layout.html")
-//     if err != nil {
-//         http.Error(w, err.Error(), http.StatusInternalServerError)
-//         return
-//     }
-
-//     conf.Title = "DNSCrypt-proxy"
-//     tmpl.Execute(w, conf)
-// }
-
 func renderhtml() {
     var conf Config
     // if _, err := toml.DecodeFile("example-dnscrypt-proxy.toml", &conf); err != nil { // for development
     if _, err := toml.DecodeFile("/var/packages/dnscrypt-proxy/target/var/dnscrypt-proxy.toml", &conf); err != nil {
-        fmt.Println(err)
-        return
+        logError(err.Error())
     }
 
     tmpl, err := template.ParseFiles("layout.html")
     if err != nil {
-        fmt.Println(err)
-        return
+        logError(err.Error())
     }
 
     conf.Title = "DNSCrypt-proxy"
+    fmt.Println("Status: 200 OK\nContent-Type: text/html; charset=utf-8\n\n")
     tmpl.Execute(os.Stdout, conf)
-    if err != nil { panic(err) }
+    if err != nil {
+        logError(err.Error())
+    }
 }
 
 func token() (string, error) {
@@ -77,20 +46,21 @@ func token() (string, error) {
     }
 
     // Content-Type: text/html [..] { "SynoToken" : "GqHdJil0ZmlhE", "result" : "success", "success" : true }
-    r := regexp.MustCompile("SynoToken\" *: *\"([^\"]+)\"")
+    r, err := regexp.Compile("SynoToken\" *: *\"([^\"]+)\"")
+    if err != nil {
+        return string(cmdOut), err
+    }
     token := r.FindSubmatch(cmdOut)
     if len(token) < 1 {
-        return "", errors.New("no token found!")
+        return string(cmdOut), errors.New("Sorry, you need to login first!")
     }
     return string(token[1]), nil
 }
 
 func auth() (string) {
     token, err := token()
-     if err != nil {
-        dd(err.Error())
-        // Todo return 404
-        // Todo return 401
+    if err != nil {
+        logUnauthorised(err.Error())
     }
 
     // X-SYNO-TOKEN:9WuK4Cf50Vw7Q
@@ -99,23 +69,23 @@ func auth() (string) {
     cmd := exec.Command("/usr/syno/synoman/webman/modules/authenticate.cgi")
     cmdOut, err := cmd.Output()
     if err != nil {
-        dd(err.Error()+" | "+string(cmdOut))
-        // Todo return 404
-        // Todo return 401
+        logUnauthorised(err.Error())
     }
     return string(cmdOut)
 }
 
-func dd(str string) { // dump and die
-    // fmt.Println("Status: 200 OK\nContent-Type: text/html; charset=utf-8\n\n<!DOCTYPE html>\n<html><head><title>DNSCrypt-proxy - dump and die</title></head><body><p>")
-    fmt.Println("<p>")
+func logError(str string) { // dump and die
+    fmt.Println("Status: 500 Internal server error\nContent-Type: text/html; charset=utf-8\n\n")
     fmt.Println(str)
-    fmt.Println("</p></body></html>")
+    os.Exit(0)
+}
+func logUnauthorised(str string) { // dump and die
+    fmt.Println("Status: 401 Unauthorized\nContent-Type: text/html; charset=utf-8\n\n")
+    fmt.Println(str)
     os.Exit(0)
 }
 
 func main() {
-    fmt.Println("Status: 200 OK\nContent-Type: text/html; charset=utf-8\n\n<!DOCTYPE html><html><head><title>DNSCrypt-proxy</title></head><body>")
     // Todo:
     // parse POST params
     // save pref changes
@@ -127,15 +97,8 @@ func main() {
     // send csrf token
     // fix image icons
 
-
-    auth()
-    renderhtml()
     // user := auth()
     // dd("user: "+user)
-
-    // if err := cgi.Serve(http.HandlerFunc(cgihandler)); err != nil {
-    //     fmt.Println(err)
-    // }
-
-    fmt.Println("</body></html>")
+    auth()
+    renderhtml()
 }

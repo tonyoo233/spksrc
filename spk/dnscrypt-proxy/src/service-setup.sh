ca59144848c396110542f57a2d3a91b4d7880d0d
Sebastian Schmidt <publicarray@users.noreply.github.com>
2018-04-18 23:52:04 +1000

    code review, default port to 53
    Automate port switching i.e. dnsmasq/dhcpd config

--- spk/dnscrypt-proxy/src/service-setup.sh
+++ spk/dnscrypt-proxy/src/service-setup.sh
@@ -3,13 +3,22 @@ DNSCRYPT_PROXY="${SYNOPKG_PKGDEST}/bin/dnscrypt-proxy"
 PID_FILE="${SYNOPKG_PKGDEST}/var/dnscrypt-proxy.pid"
 CFG_FILE="${SYNOPKG_PKGDEST}/var/dnscrypt-proxy.toml"
 EXAMPLE_FILES="${SYNOPKG_PKGDEST}/example-*"
+# SERVICE_COMMAND="${DNSCRYPT_PROXY} --config ${CFG_FILE} --pidfile ${PID_FILE}"
+# SVC_BACKGROUND=y
 
-SERVICE_COMMAND="${DNSCRYPT_PROXY} --config ${CFG_FILE} --pidfile ${PID_FILE}"
-SVC_BACKGROUND=y
+service_prestart () {
+    echo "Free port 53 from dnsmasq" >> "${LOG_FILE}"
+    echo 'enable="yes"' > /etc/dhcpd/dhcpd-custom-custom.info
+    /etc/rc.network nat-restart-dhcp >> "${LOG_FILE}" 2>&1
+    ${DNSCRYPT_PROXY} --config "${CFG_FILE}" --pidfile "${PID_FILE}" --logfile "${LOG_FILE}" &
+}
 
-EFF_USER=root # fix permissions
+service_poststop () {
+    echo "Enable port 53 on dnsmasq" >> "${LOG_FILE}"
+    echo 'enable="no"' > /etc/dhcpd/dhcpd-custom-custom.info
+    /etc/rc.network nat-restart-dhcp >> "${LOG_FILE}" 2>&1
+}
 
-# rm -drf work-ipq806x-1.1/scripts && make arch-ipq806x-1.1
 service_postinst () {
     echo "Running post-install script" >> "${INST_LOG}"
     mkdir -p "${SYNOPKG_PKGDEST}"/var >> "${INST_LOG}" 2>&1
@@ -29,44 +38,36 @@ service_postinst () {
         listen_addresses=\[${wizard_listen_address:-"'0.0.0.0:$SERVICE_PORT'"}\]
         server_names=\[${wizard_servers:-"'scaleway-fr', 'google', 'yandex', 'cloudflare'"}\]
 
-        # change default setting
+        # change default settings
         sed -i -e "s/listen_addresses = .*/listen_addresses = ${listen_addresses}/" \
             -e "s/require_dnssec = .*/require_dnssec = true/" \
             -e "s/# server_names = .*/${server_names_enabled:-""}server_names = ${server_names}/" \
             -e "s/ipv6_servers = .*/ipv6_servers = ${wizard_ipv6:=false}/" \
-            -e "s@# log_file = 'dnscrypt-proxy.log'@log_file = '/var/dnscrypt-proxy.log'@" \
             "${CFG_FILE}" >> "${INST_LOG}" 2>&1
     fi
 
     echo "Setting up the Web GUI..." >> "${INST_LOG}"
     ln -s "${SYNOPKG_PKGDEST}/ui" /usr/syno/synoman/webman/3rdparty/dnscrypt-proxy >> "${INST_LOG}" 2>&1
 
-
-# fix permissions
-# -> need to run as root for port 53
-    echo "Fixing permissions..." >> "${INST_LOG}"
-
+    echo "Fixing permissions for cgi GUI..." >> "${INST_LOG}"
     ## Allow cgi user to write to this file
-    # chown dosn't work as it's overwritten see page 104 in https://developer.synology.com/download/developer-guide.pdf
+    # chown dosn't work as it's overwritten. see page 104 in https://developer.synology.com/download/developer-guide.pdf
     # chown system /var/packages/dnscrypt-proxy/target/var/dnscrypt-proxy.toml
     # Less than ideal solution, ToDo: find something better
     chmod 0666 "${SYNOPKG_PKGDEST}/var/dnscrypt-proxy.toml" >> "${INST_LOG}" 2>&1
     chmod 0666 "${SYNOPKG_PKGDEST}"/var/*.txt >> "${INST_LOG}" 2>&1
 
-    synouser --add "dnscrypt" "" "DNSCrypt-proxy" 0 "" 0 >> ${INST_LOG} 2>&1
-    synouser --rebuild all >> ${INST_LOG} 2>&1
-    synogroup --add "dnscrypt" "dnscrypt" >> ${INST_LOG} 2>&1
-    synogroup --descset "dnscrypt" "DNSCrypt-proxy" >> ${INST_LOG} 2>&1
-    synogroup --rebuild all >> ${INST_LOG} 2>&1
-    EFF_USER=dnscrypt
-
-    # chown -hR "dnscrypt:dnscrypt" >> "${INST_LOG}" 2>&1 # gets overwritten in 'installer' script
+    echo "Set dnsmasq settings" >> "${INST_LOG}"
+    echo 'port=0' > /etc/dhcpd/dhcpd-custom-custom.conf
+    echo 'enable="yes"' > /etc/dhcpd/dhcpd-custom-custom.info
+    /etc/rc.network nat-restart-dhcp >> "${INST_LOG}" 2>&1
 }
 
 service_postuninst () {
     rm -f /usr/syno/synoman/webman/3rdparty/dnscrypt-proxy >> "${INST_LOG}" 2>&1
-    synouser --del "dnscrypt"
-    synogroup --del "dnscrypt"
-    synouser --rebuild all
-    synogroup --rebuild all
+    echo "Enable port 53 on dnsmasq" >> "${INST_LOG}"
+    rm -f /etc/dhcpd/dhcpd-custom-custom.conf
+    rm -f /etc/dhcpd/dhcpd-custom-custom.info
+    /etc/rc.network nat-restart-dhcp
 }
+# rm -drf work-ipq806x-1.1/scripts && make arch-ipq806x-1.1

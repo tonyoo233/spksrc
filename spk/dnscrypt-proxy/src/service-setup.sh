d4c5f07d1515a83540968c52e55500c54145e835
Sebastian Schmidt <publicarray@users.noreply.github.com>
2018-10-23 16:58:29 +1100

    Use the cached file to fix the 0 byte file on upgrade

--- spk/dnscrypt-proxy/src/service-setup.sh
+++ spk/dnscrypt-proxy/src/service-setup.sh
@@ -130,3 +130,8 @@ service_postuninst () {
     rm -f /etc/dhcpd/dhcpd-dnscrypt-dnscrypt.conf
     rm -f /etc/dhcpd/dhcpd-dnscrypt-dnscrypt.info
 }
+
+service_postupgrade () {
+    # fix upgrade from 2.0.17-2 to 2.0.17_3
+    cp -f "${SYNOPKG_PKGDEST}"/offline-cache/generate-domains-blacklist.py "${SYNOPKG_PKGDEST}/var/" >> "${INST_LOG}" 2>&1
+}

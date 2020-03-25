e7a06e65fb3df078b07f43ece398374fab7bc154
Sebastian Schmidt <publicarray@users.noreply.github.com>
2020-03-25 20:42:23 +1100

    Set jellyfin storage dirs

--- spk/jellyfin/src/service-setup.sh
+++ spk/jellyfin/src/service-setup.sh
@@ -1,7 +1,8 @@
 # Package specific behaviours
 # Sourced script by generic installer and start-stop-status scripts
 
-SERVICE_COMMAND="${SYNOPKG_PKGDEST}/jellyfin"
+SERVICE_COMMAND="${SYNOPKG_PKGDEST}/jellyfin" -d ${SYNOPKG_PKGDEST}/var/data -C ${SYNOPKG_PKGDEST}/var/cache -c ${SYNOPKG_PKGDEST}/var/config -l ${SYNOPKG_PKGDEST}/var/log -w ${SYNOPKG_PKGDEST}/web
+
 # CFG_FILE="${SYNOPKG_PKGDEST}/var/jellyfin.yml"
 SVC_BACKGROUND=y
 SVC_WRITE_PID=y

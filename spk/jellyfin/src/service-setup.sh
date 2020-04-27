f3709acf3e587a4835650edd4139c74d288ecb48
Sebastian Schmidt <publicarray@users.noreply.github.com>
2020-04-27 21:29:36 +1000

    Add Github Actions

--- spk/jellyfin/src/service-setup.sh
+++ spk/jellyfin/src/service-setup.sh
@@ -4,11 +4,6 @@
 # https://github.com/dotnet/core/issues/4011
 [ -z "$DOTNET_BUNDLE_EXTRACT_BASE_DIR" ] && export DOTNET_BUNDLE_EXTRACT_BASE_DIR="${XDG_CACHE_HOME:-"/var/packages/jellyfin/target/var/"}/dotnet_bundle_extract"
 
-# fix Error processing request: /volume1/@appstore/jellyfin/var/data/transcodes does not exist.
-# Why doesn't jellyfin create the folder since it is the default path?
-mkdir -p /volume1/@appstore/jellyfin/var/data/transcodes
-chown sc-jellyfin:jellyfin /volume1/@appstore/jellyfin/var/data/transcodes
-
 SERVICE_COMMAND="${SYNOPKG_PKGDEST}/jellyfin --noautorunwebapp --package-name synology -d ${SYNOPKG_PKGDEST}/var/data -C ${SYNOPKG_PKGDEST}/var/cache -c ${SYNOPKG_PKGDEST}/var/config -l ${SYNOPKG_PKGDEST}/var/log -w ${SYNOPKG_PKGDEST}/web"
 
 SVC_BACKGROUND=y

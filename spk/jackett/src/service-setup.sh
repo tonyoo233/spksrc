5fac4089f4fe2d2234cd5e8d45a16391333ef39c
Sebastian Schmidt <publicarray@users.noreply.github.com>
2020-08-10 21:35:18 +1000

    Jackett standalone AMDx64

--- spk/jackett/src/service-setup.sh
+++ spk/jackett/src/service-setup.sh
@@ -5,19 +5,12 @@
 service_prestart ()
 {
     # Replace generic service startup
-    MONO_PATH="/var/packages/mono/target/bin"
-    PATH="${SYNOPKG_PKGDEST}/bin:${MONO_PATH}:${PATH}"
-    MONO="${MONO_PATH}/mono"
-    JACKETT="${SYNOPKG_PKGDEST}/share/${SYNOPKG_PKGNAME}/JackettConsole.exe"
+    PATH="${SYNOPKG_PKGDEST}/bin:${PATH}"
+    JACKETT="${SYNOPKG_PKGDEST}/share/${SYNOPKG_PKGNAME}/jackett"
     HOME_DIR="${SYNOPKG_PKGDEST}/var"
 
-    # workaround for mono bug with armv5 (https://github.com/mono/mono/issues/12537)
-    if [ "$SYNOPKG_DSM_ARCH" == "88f8621" -o "$SYNOPKG_DSM_ARCH" == "88f8622" ]; then
-        MONO="MONO_ENV_OPTIONS='-O=-aot,-float32' ${MONO_PATH}/mono"
-    fi
-
     echo "Starting Jackett as user ${EFF_USER}" >> ${LOG_FILE}
-    COMMAND="env HOME=${HOME_DIR} PATH=${PATH} LD_LIBRARY_PATH=${SYNOPKG_PKGDEST}/lib ${MONO} ${JACKETT} --PIDFile ${PID_FILE}"
+    COMMAND="env HOME=${HOME_DIR} PATH=${PATH} LD_LIBRARY_PATH=${SYNOPKG_PKGDEST}/lib ${JACKETT} --PIDFile ${PID_FILE}"
 
     if [ $SYNOPKG_DSM_VERSION_MAJOR -lt 6 ]; then
         su ${EFF_USER} -s /bin/sh -c "${COMMAND}" >> ${LOG_FILE} 2>&1 &
